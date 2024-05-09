727441411fdc073c541327148d0bd99163939193
petepresh007 <preshtech18@gmail.com>
2024-05-08 21:57:15 -0800

    fixing

--- src/pages/news.jsx
+++ src/pages/news.jsx
@@ -1,7 +1,7 @@
 import {useParams} from "react-router-dom";
 import {useState, useEffect} from "react";
 import axios from "axios";
-import {football, url} from "../server"
+import {football, url} from "../server";
 
 export const News = () =>{
     const {id} = useParams();
